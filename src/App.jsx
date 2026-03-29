b7d46fd6489915904b9bab2302903f957d6f1d54
jbile <jbilewicz@op.pl>
2026-03-29 12:09:48 +0200

    New chapter

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import Food from './Food.jsx'
 import Card from './Card.jsx'
+import Button from './Button.jsx'
+import Student from './Students.jsx'
+import UserGreeting from './UserGreeting.jsx'
 
 function App() {
     return(
       <>
       <Header/>
       <Food/>
+      <Student name="Spongebob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={20} isStudent={false}/>
+      <Student />
+      <UserGreeting isLoggedIn = {true} username ="Jacob"/>
       <Card/>
+      <Button/>
       <Footer/>
       </>  
     );
